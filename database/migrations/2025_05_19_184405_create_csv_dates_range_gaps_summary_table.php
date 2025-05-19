47971454478d99a69e4ea944bc39dd3de2e5606d
Robert <robert.deabreu@borrox.com>
2025-05-20 01:25:43 +0200

    New migrations for the filters

---
+++ database/migrations/2025_05_19_184405_create_csv_dates_range_gaps_summary_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCsvDatesRangeGapsSummaryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('csv_dates_range_gaps_summary', function (Blueprint $table) {
+            $table->unsignedBigInteger('csv_uploads_id');
+            $table->unsignedBigInteger('csv_id');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->integer('total_errors');
+            $table->primary(['csv_uploads_id', 'csv_id']);
+            $table->index(['csv_uploads_id', 'csv_id']);
+            $table->index('start_date');
+            $table->index('end_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('csv_dates_range_gaps_summary');
+    }
+}

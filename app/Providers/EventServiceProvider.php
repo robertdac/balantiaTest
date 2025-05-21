e3a06c981fdfc38c8ca41fd437449ec27a22a49b
Robert <robert.deabreu@borrox.com>
2025-05-21 09:44:32 +0200

    register event and listener

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -1,32 +1,40 @@
 <?php
 
-namespace App\Providers;
+    namespace App\Providers;
 
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
-use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
+    use App\Events\CsvUploadStatusChanged;
+    use App\Events\FileCreated;
+    use App\Listeners\HandleCsvUploadStatusChange;
+    use App\Listeners\SplitCsv;
+    use Illuminate\Auth\Events\Registered;
+    use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+    use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+    use Illuminate\Support\Facades\Event;
 
-class EventServiceProvider extends ServiceProvider
-{
-    /**
-     * The event listener mappings for the application.
-     *
-     * @var array<class-string, array<int, class-string>>
-     */
-    protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
-    ];
-
-    /**
-     * Register any events for your application.
-     *
-     * @return void
-     */
-    public function boot()
+    class EventServiceProvider extends ServiceProvider
     {
-        //
+        /**
+         * The event listener mappings for the application.
+         *
+         * @var array<class-string, array<int, class-string>>
+         */
+        protected $listen = [
+            Registered::class => [
+                SendEmailVerificationNotification::class,
+            ],
+            CsvUploadStatusChanged::class => [
+                HandleCsvUploadStatusChange::class,
+            ],
+
+        ];
+
+        /**
+         * Register any events for your application.
+         *
+         * @return void
+         */
+        public function boot()
+        {
+            //
+        }
     }
-}
